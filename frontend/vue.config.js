3fe0f8c3d44bc6a7f395c5c244518dc7d749d53f
TITHDAVID-RA <tithdavidra2457@gmail.com>
2026-06-21 23:32:18 +0700

    v3

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -16,5 +16,28 @@ module.exports = defineConfig({
       args[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = JSON.stringify(false)
       return args
     })
+
+    const copyPlugin = config.plugin('copy')
+    copyPlugin.tap(args => {
+      const [options] = args
+      if (options && Array.isArray(options.patterns)) {
+        options.patterns = options.patterns.map(pattern => {
+          if (pattern.globOptions) {
+            pattern.globOptions = {
+              ...pattern.globOptions,
+              ignore: Array.isArray(pattern.globOptions.ignore)
+                ? [...new Set([...pattern.globOptions.ignore, '**/index.html'])]
+                : ['**/index.html']
+            }
+          } else {
+            pattern.globOptions = {
+              ignore: ['**/index.html']
+            }
+          }
+          return pattern
+        })
+      }
+      return args
+    })
   }
 })
